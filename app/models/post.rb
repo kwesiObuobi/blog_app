f1a6ee4069a2cddea5891c225fe5c183227efe47
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-12 17:16:34 +0000

    Add required methods for Users and Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
+
+  def update_posts_counter
+    count = if author.posts_counter.nil?
+              0
+            else
+              author.posts_counter
+            end
+    author.posts_counter = count + 1
+    author.save
+  end
 end
