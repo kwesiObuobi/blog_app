a29c8859f8e449fa6c16d91cce980da3bc45d50b
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-12 10:26:26 +0000

    Add Comments migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_102413) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_102548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.bigint "author_id", null: false
+    t.bigint "posts_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -34,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_102413) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
