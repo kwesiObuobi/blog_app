362ba7df98cc5be9d42ba880cf41ce4d3ec65a61
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-13 09:44:42 +0000

    Add custom methods to update post counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
+  before_save :update_posts_counter
+
   def update_posts_counter
     count = if author.posts_counter.nil?
               0
@@ -11,5 +13,9 @@ class Post < ApplicationRecord
             end
     author.posts_counter = count + 1
     author.save
+  end
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
