9735d433f4cc602dff06aeba2ec9211ca9598c77
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-14 15:01:32 +0000

    Add required validations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
+  validates :title, presence: true, length: { in: 1..250 }
+  validate :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   before_save :update_posts_counter
 
   def update_posts_counter
