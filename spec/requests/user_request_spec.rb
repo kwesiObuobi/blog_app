52cbdfb320d26a7232f6f796b1ab9473074a6e1d
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-17 15:35:12 +0000

    Add gem 'rails-controller-testing' to the Gemfile

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -7,7 +7,7 @@ describe User, type: :request do
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
       expect(response).to render_template('users/index')
-      expect(response.body).to eq('Here is a list of all users')
+      expect(response.body).to include('Here is a list of all users')
     end
   end
 end
