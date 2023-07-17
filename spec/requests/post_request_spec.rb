af6637c4092e4061ef9eba1165d536dc1b90df5c
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-17 15:41:22 +0000

    Add request-spec for posts_controller's 'index' action

---
+++ spec/requests/post_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Post, type: :request do
+  describe 'routes with post_controller' do
+    it 'should render the index action correctly' do
+      get '/users/:user_id/posts'
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(response).to render_template('posts/index')
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+end
