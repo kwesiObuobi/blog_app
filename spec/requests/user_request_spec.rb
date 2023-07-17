e1f26f14fc8b83582239517d7af2ddd67fdb6a9a
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-17 15:37:18 +0000

    Add request_spec for users_controller's 'show' action

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -9,5 +9,13 @@ describe User, type: :request do
       expect(response).to render_template('users/index')
       expect(response.body).to include('Here is a list of all users')
     end
+
+    it 'should render the show action correctly' do
+      get '/users/:id'
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+      expect(response).to render_template('users/show')
+      expect(response.body).to include('Here is a details page for a given user')
+    end
   end
 end
