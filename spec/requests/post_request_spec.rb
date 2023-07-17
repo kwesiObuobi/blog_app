7cc7c51c944e3d9bb913892bc54b17347291ef75
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-17 15:42:34 +0000

    Add request-spec for posts_controller's 'show' action

--- spec/requests/post_request_spec.rb
+++ spec/requests/post_request_spec.rb
@@ -9,5 +9,13 @@ describe Post, type: :request do
       expect(response).to render_template('posts/index')
       expect(response.body).to include('Here is a list of posts for a given user')
     end
+
+    it 'should render the show action correctly' do
+      get '/users/:user_id/posts/id'
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(response).to render_template('posts/show')
+      expect(response.body).to include('specific post for a given user')
+    end
   end
 end
