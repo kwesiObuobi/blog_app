016bf6498424d3d7624527a0635c5288bc072528
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-20 12:01:20 +0000

    Add styling for 'users/user_id/posts/id'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,8 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
 end
