d57747d9b37b143f6928970eaec5ecec8d709ec7
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-20 09:56:59 +0000

    Add styling for 'users/id'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @recent_posts = User.three_recent_posts(@user)
+  end
 end
