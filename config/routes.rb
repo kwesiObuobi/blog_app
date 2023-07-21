b1e44e9db87d2f24c948ce36ea8d23bed9afc77f
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-21 14:49:36 +0000

    Add new.html.erb and its routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   root 'users#index'
   get '/users/:id', to: 'users#show'
 
+  get '/users/new/posts', to: 'posts#new'
+
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
+
 end
