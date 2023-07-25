87afdd87ca50635dd3d906bd2e535b7fabf1b273
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-07-25 21:14:52 +0300

    Add More test in User Show

--- spec/features/users_system_spec.rb
+++ spec/features/users_system_spec.rb
@@ -70,6 +70,16 @@ RSpec.describe 'User', type: :feature do
     it 'I can see the user first 3 posts.' do
       expect(page).to_not have_content 'post content 4'
     end
-   
+    it 'I can see a button that lets me view all of a users posts.' do
+      expect(page).to have_content 'See all posts'
+    end
+    it 'When I click a users post, it redirects me to that posts show page.' do
+      visit "/users/#{@abelt.id}/posts/#{@post1.id}"
+      expect(page).to have_content 'post content 1'
+    end
+    it 'When I click to see all posts, it redirects me to the users posts index page.' do
+      click_link 'See all posts'
+      expect(page).to have_content 'post 1 title'
+    end
   end
 end
